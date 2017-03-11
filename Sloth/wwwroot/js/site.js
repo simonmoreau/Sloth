f17cced6bfef8471faccc4646824f646063f5ea1
Simon Moreau <simon@bim42.com>
2017-03-11 18:22:00 +0100

    Initial setup Adding Material Design Lite

--- Sloth/wwwroot/js/site.js
+++ Sloth/wwwroot/js/site.js
@@ -1,1 +1,6 @@
 ﻿// Write your Javascript code.
+
+//Display file name in the upload form
+document.getElementById("uploadBtn").onchange = function () {
+    document.getElementById("uploadFile").value = this.files[0].name;
+};
---
+++ Sloth/wwwroot/js/site.min.js
@@ -0,0 +1,1 @@
+document.getElementById("uploadBtn").onchange=function(){document.getElementById("uploadFile").value=this.files[0].name};
