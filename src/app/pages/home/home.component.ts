2d027bb6567bd5c7296c0c3fc41d5b532664746b
Simon Moreau <simon@bim42.com>
2022-09-08 08:39:36 +0200

    read a BCF file

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import { NgModule, Component, enableProdMode } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, SafeUrl } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { BcfReader, Topic } from '@parametricos/bcf-js';
+import { DomSanitizer } from '@angular/platform-browser';
 
 if (!/localhost/.test(document.location.host)) {
   enableProdMode();
 }
+
 
 @Component({
   templateUrl: 'home.component.html',
@@ -14,10 +16,10 @@ if (!/localhost/.test(document.location.host)) {
 export class HomeComponent {
   topics: Topic[] = [];
   imageItemToDisplay: any = {};
-
+  
   popupVisible = false;
 
-  constructor() {
+  constructor(private sanitizer: DomSanitizer) {
   }
 
   displayImagePopup(e: any) {
@@ -36,7 +38,53 @@ export class HomeComponent {
         await reader.read(await file.arrayBuffer());
 
         this.topics = reader.topics;
+        
+        this.topics.forEach(t=>{
+          if (t.viewpoints.length > 0)
+          {
+            console.log(t.viewpoints[0].snapshot)
+          }
+        })
       }
     }
+  }
+
+  private sanitize(url: string) {
+    return this.sanitizer.bypassSecurityTrustUrl(url);
+  }
+
+  private arrayBufferToBase64(buffer: ArrayBuffer | undefined) : string {
+    if (!buffer) return ''
+    var binary = '';
+    var bytes = new Uint8Array(buffer);
+    var len = bytes.byteLength;
+    for (var i = 0; i < len; i++) {
+      binary += String.fromCharCode(bytes[i]);
+    }
+    return window.btoa(binary);
+  }
+
+  topicToImage(topic: Topic) : SafeUrl | undefined{
+    if ( topic.viewpoints.length > 0)
+    {
+      const arrayBuffer = topic.viewpoints[0].snapshot
+      const b64 = this.arrayBufferToBase64(arrayBuffer)
+
+      return this.sanitize('data:image/jpg;base64, ' + b64);
+    }
+    else
+    {
+      return;
+    }
+  }
+}
+
+export class DisplayedTopic {
+  topic: Topic;
+  image: Promise<SafeUrl | undefined>;
+
+  constructor(topic : Topic, image : Promise<SafeUrl| undefined>) {
+    this.topic = topic;
+    this.image = image;
   }
 }
